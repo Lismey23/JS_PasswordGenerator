1b938de2ea5721ff548bd7e76d9332ddb94e40ce
Lismey Plasencia <lismeyplasencia@gmail.com>
2021-01-03 14:37:37 -0500

    created the password length prompt

--- script.js
+++ script.js
@@ -10,5 +10,42 @@ function writePassword() {
 
 }
 
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+generateBtn.onclick = function generatePassword() {
+
+var passwordLength = window.prompt("How long do you want your password to be? It must be between 8 and 128 characters ");
+if (passwordLength <=7 || passwordLength >= 128) {
+  alert("Your password must be between 8 and 128 characters");
+  return;
+} 
+else {
+  alert ("Your password is "  +  passwordLength+  "  characters long");
+}
+
+
+
+
+
+var characterTypes = {
+  lowercase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  uppercase: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
+  numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+  specialcharacters:['!@#$%^&*()_+{}:"<>?\|[];\',./`~'],
+}
+
+var characterUserChoice = function () {
+  var userLowerCase = prompt ("Please confirm with a yes or no if you would like to add Lower cases to your password");
+  var userUpperCase = prompt ("Please confirm with a yes or no if you would like to add Upper cases to your password");
+  var userNumber = prompt ("Please confirm with a yes or no if you would like to add Numbers to your password");
+  var userSpecialCharacters = prompt ("Please confirm with a yes or no if you would like to add Special Characters to your password");
+}
+
+}
+
+generatePassword()
+characterUserChoice ()
