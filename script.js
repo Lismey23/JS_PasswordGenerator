1f1bff8ceb52e543a4ef20b74a98c9b2789e6ef7
Lismey Plasencia <lismeyplasencia@gmail.com>
2021-01-08 12:56:35 -0500

    updated Readme and JS file

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ function writePassword() {
 function generatePassword() {
 
 // What characters would the user lke to include
-var lowerCaseChoice = confirm ("Would you like to include lower case characters in your password ?")
-var upperCaseChoice = confirm ("Would you like to include upper case characters in your password ?")
+var lowerCaseChoice = confirm ("Would you like to include lowercase characters in your password ?")
+var upperCaseChoice = confirm ("Would you like to include uppercase characters in your password ?")
 var numbersChoice = confirm ("Would you like to include numbers in your password ?")
 var specCharChoice = confirm ("Would you like to include special characters in your password ?")
 
@@ -35,16 +35,15 @@ if (lowerCaseChoice === false && upperCaseChoice === false && numbersChoice === 
 
 
 
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialcharacters = ["+", "-", "&&", "||", "!", "(", ")", "{", "}", "[", "]", "^","~", "*", "?", ":","\"","\\"];
 
 //creating new Array to include the characters the user chooses
 
 var passwordCharacter = [];
 if (lowerCaseChoice === true) {
-
   passwordCharacter = passwordCharacter.concat(lowercase);
 }
 if (upperCaseChoice === true) {
@@ -70,7 +69,7 @@ var userPassword= "";
 for (var i = 0; i < lenght; i++){
   var char = Math.floor(Math.random()*passwordCharacter.length);
 
-  userPassword += String.fromCharCode(char)
+  userPassword += String.fromCharCode(char);
   }
 return userPassword;
 }
